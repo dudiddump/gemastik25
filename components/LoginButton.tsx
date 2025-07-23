cabb943dd8cee8f2369ff721b7f40b1c246b4f69
sryaerlmba <suryaerlamba3@gmail.com>
2025-07-23 21:12:42 +0700

    buat komponen login button

---
+++ components/LoginButton.tsx
@@ -0,0 +1,7 @@
+export default function LoginButton() {
+  return (
+    <div className="bg-primary px-6 py-2 rounded-sm lg:block hidden cursor-pointer">
+      <p className="font-semibold">Login</p>
+    </div>
+  );
+}
