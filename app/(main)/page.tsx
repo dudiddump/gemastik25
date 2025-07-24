998f72ce210a001e5d917701b640e871165255bf
sryaerlmba <suryaerlamba3@gmail.com>
2025-07-24 09:49:46 +0700

    page baru

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -2,8 +2,10 @@ import ResponsiveContainer from "@/components/ResponsiveContainer";
 
 export default function Home() {
   return (
+    // responsive container ini adalah komponen kontainer yang udah responsive dan tinggal pakke
     <ResponsiveContainer className="mt-30">
       {/* ini pecah aja kedalam komponen hero ya */}
+
       <h1>Hero Section</h1>
     </ResponsiveContainer>
   );
