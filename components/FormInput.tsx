2d698b19f3e2263d22d4fa74571db472533e2706
sryaerlmba <suryaerlamba3@gmail.com>
2025-07-24 20:13:11 +0700

    komponen form input

--- components/FormInput.tsx
+++ components/FormInput.tsx
@@ -4,12 +4,16 @@ export default function FormInput({
   type = "text",
   icon,
   autoComplete = "off",
+  name,
+  defaultValue,
 }: {
   label: string;
   placeholder: string;
   type?: string;
   icon?: React.ReactNode;
   autoComplete?: string;
+  name: string;
+  defaultValue?: string;
 }) {
   return (
     <div className="flex flex-col gap-2">
@@ -18,11 +22,13 @@ export default function FormInput({
       <div className="flex items-center gap-2 px-3 py-2 border border-gray-300 rounded-md focus-within:ring-2 focus-within:ring-[#55daa9] transition">
         {icon}
         <input
+          name={name}
           type={type}
           autoComplete={autoComplete}
           placeholder={placeholder}
           className="w-full outline-none text-sm text-gray-800 bg-transparent"
           required
+          defaultValue={defaultValue}
         />
       </div>
     </div>
---
+++ components/FormInputPassword.tsx
@@ -0,0 +1,49 @@
+import { Lock, Eye, EyeOff } from "lucide-react";
+import { useState } from "react";
+
+export default function FormInputPassword({
+  label,
+  placeholder,
+  autoComplete = "off",
+  name,
+}: {
+  label: string;
+  placeholder: string;
+  autoComplete?: string;
+  name: string;
+}) {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleClick = () => {
+    setShowPassword(!showPassword);
+  };
+
+  return (
+    <div className="flex flex-col gap-2">
+      <label className="text-sm text-gray-600">{label}</label>
+
+      <div className="flex items-center gap-2 px-3 py-2 border border-gray-300 rounded-md focus-within:ring-2 focus-within:ring-[#55daa9] transition">
+        <Lock className="w-4 h-4 text-gray-800" />
+        <input
+          type={showPassword ? "text" : "password"}
+          autoComplete={autoComplete}
+          placeholder={placeholder}
+          className="w-full outline-none text-sm text-gray-800 bg-transparent"
+          required
+          name={name}
+        />
+        {showPassword ? (
+          <Eye
+            className="w-4 h-4 text-gray-800 cursor-pointer"
+            onClick={handleClick}
+          />
+        ) : (
+          <EyeOff
+            className="w-4 h-4 text-gray-800 cursor-pointer"
+            onClick={handleClick}
+          />
+        )}
+      </div>
+    </div>
+  );
+}
