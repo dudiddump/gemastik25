69ddf692409cafa86e1d0e0bd1590478341268f1
sryaerlmba <suryaerlamba3@gmail.com>
2025-07-24 09:41:43 +0700

    tambah page di main halaman dan layout

---
+++ app/(main)/page.tsx
@@ -0,0 +1,10 @@
+import ResponsiveContainer from "@/components/ResponsiveContainer";
+
+export default function Home() {
+  return (
+    <ResponsiveContainer className="mt-30">
+      {/* ini pecah aja kedalam komponen hero ya */}
+      <h1>Hero Section</h1>
+    </ResponsiveContainer>
+  );
+}
--- app/coba/page.tsx
+++
@@ -1,5 +0,0 @@
-export default function coba() {
-    return (
-    <p>ini halaman coba</p>
-    )
-}
