c6591215bb09cb8356cfe2941d78944df53aff83
sryaerlmba <suryaerlamba3@gmail.com>
2025-07-27 21:05:42 +0700

    buat theme

---
+++ components/ClientThemeHandler.tsx
@@ -0,0 +1,19 @@
+"use client";
+import { useEffect } from "react";
+
+export default function ClientThemeHandler() {
+  useEffect(() => {
+    const storedTheme = localStorage.getItem("theme");
+    const systemPrefersDark = window.matchMedia(
+      "(prefers-color-scheme: dark)"
+    ).matches;
+
+    if (storedTheme === "dark" || (!storedTheme && systemPrefersDark)) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, []);
+
+  return null;
+}
