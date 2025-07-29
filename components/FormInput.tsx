e4f0954d9786438d9b11c758064203eb5a813ad9
sryaerlmba <suryaerlamba3@gmail.com>
2025-07-29 20:49:37 +0700

    simplify form prosp

--- components/FormInput.tsx
+++ components/FormInput.tsx
@@ -1,34 +1,18 @@
-export default function FormInput({
-  label,
-  placeholder,
-  type = "text",
-  icon,
-  autoComplete = "off",
-  name,
-  defaultValue,
-}: {
+type FormInputProps = React.InputHTMLAttributes<HTMLInputElement> & {
   label: string;
-  placeholder: string;
-  type?: string;
   icon?: React.ReactNode;
-  autoComplete?: string;
-  name: string;
-  defaultValue?: string;
-}) {
+};
+
+export default function FormInput({ label, icon, ...props }: FormInputProps) {
   return (
     <div className="flex flex-col gap-2 mb-2">
       <label className="text-sm text-gray-600">{label}</label>
-
       <div className="flex items-center gap-2 px-3 py-2 border border-gray-300 rounded-md focus-within:ring-2 focus-within:ring-[#55daa9] transition">
         {icon}
         <input
-          name={name}
-          type={type}
-          autoComplete={autoComplete}
-          placeholder={placeholder}
           className="w-full outline-none text-sm text-gray-800 bg-transparent dark:text-gray-400"
           required
-          defaultValue={defaultValue}
+          {...props}
         />
       </div>
     </div>
